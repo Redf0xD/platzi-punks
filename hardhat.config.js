16da358a2dcb9c80dd1a9828e9db44781202247a
Redf0xD <ricardoprato36@gmail.com>
2022-09-01 17:48:26 -0400

    :wrench: add hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require('@nomicfoundation/hardhat-toolbox')
+require('dotenv').config()
+
+const { DEPLOYER_SIGNER_PRIVATE_KEY, INFURA_PROJECT_ID } = process.env
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: '0.8.9',
+  networks: {
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [
+        DEPLOYER_SIGNER_PRIVATE_KEY
+      ]
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [
+        DEPLOYER_SIGNER_PRIVATE_KEY
+      ]
+    }
+  }
+}
